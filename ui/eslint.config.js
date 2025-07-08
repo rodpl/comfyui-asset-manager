9771bdbf7cd3d1cec34cbdfe8b10608641867fa2
Daniel Dabrowski <106607+rodpl@users.noreply.github.com>
2025-07-09 01:06:15 +0200

    refactor: update ESLint configuration and remove unused vite.config.js

--- ui/eslint.config.js
+++ ui/eslint.config.js
@@ -6,34 +6,54 @@ import tseslint from 'typescript-eslint'
 
 export default [
   {
-    files: ['src/**/*.{js,mjs,cjs,ts,tsx}']
+    // Global ignores
+    ignores: ['dist/**', 'node_modules/**', 'coverage/**', 'test-results/**'],
   },
   {
+    // Source files
+    files: ['src/**/*.{ts,tsx}'],
     languageOptions: {
-      globals: {
-        ...globals.browser
-      },
       parser: tseslint.parser,
       parserOptions: {
         project: './tsconfig.json',
         ecmaVersion: 2020,
-        sourceType: 'module'
-      }
-    }
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
   },
+  {
+    // Vite config
+    files: ['vite.config.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.node.json',
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  // ESLint recommended configs
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
   {
+    // Custom rules
     plugins: {
-      'unused-imports': unusedImports
+      'unused-imports': unusedImports,
     },
     rules: {
       '@typescript-eslint/no-floating-promises': 'error',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/prefer-as-const': 'off',
-      'unused-imports/no-unused-imports': 'error'
-    }
-  }
+      'unused-imports/no-unused-imports': 'error',
+    },
+  },
 ]
