6c397fa2ef30750f80afbe797750d48fe971d57a
Daniel Dabrowski <106607+rodpl@users.noreply.github.com>
2025-07-09 01:06:20 +0200

    feat: update App component to display Home tab content

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css'
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <div className="tab-content">
+      <h2>Home Tab</h2>
+      <p>This is the content of the Home tab.</p>
+    </div>
+  )
 }
 
 export default App
