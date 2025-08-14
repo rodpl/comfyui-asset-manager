e7a31174fa5be30f8d423a2bab51938464279835
Daniel Dabrowski <106607+rodpl@users.noreply.github.com>
2025-08-14 03:06:14 +0200

    Task 5

--- ui/src/features/model-browser/components/index.ts
+++ ui/src/features/model-browser/components/index.ts
@@ -5,3 +5,7 @@
 export { default as ModelCard } from './ModelCard';
 export type { ModelCardProps } from './ModelCard';
 export { default as ModelCardDemo } from './ModelCardDemo';
+export { default as CivitAIBrowser } from './CivitAIBrowser';
+export type { CivitAIBrowserProps } from './CivitAIBrowser';
+export { default as CivitAISearchFilterBar } from './CivitAISearchFilterBar';
+export type { CivitAISearchFilterBarProps } from './CivitAISearchFilterBar';
